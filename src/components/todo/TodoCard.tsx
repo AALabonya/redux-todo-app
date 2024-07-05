61c70048d22f21b14db100bd6bcc1fa9858c43a6
AALabonya <1aklimakter@gmail.com>
2024-07-04 22:39:08 -0700

    added

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,5 +1,16 @@
 const TodoCard = () => {
-  return <div>TodoCard</div>;
+  return (
+    <div className="bg-white rounded-md flex justify-between items-center p-3">
+      <input type="checkbox" name="" id="" />
+      <p>Toto List</p>
+      <p>Time</p>
+      <p>Description</p>
+      <div className="flex gap-5">
+        <button>del</button>
+        <button>edit</button>
+      </div>
+    </div>
+  );
 };
 
 export default TodoCard;
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,8 +1,21 @@
+import TodoCard from "./TodoCard";
+import { Button } from "@/components/ui/button";
 const TodoContainer = () => {
   return (
-    <div>
-      <div>
-        <button></button>
+    <div className="px-2.5 py-3.5 border rounded-md">
+      <div className="flex justify-between">
+        <Button>Add todo</Button>
+        <Button>Filter</Button>
+      </div>
+      <div className="bg-primary-gradient w-full space-y-3 h-[500px] rounded-xl p-5">
+        {/* <div className="bg-white rounded-md  items-center p-3 text-center flex justify-center">
+          <p>There is task pending</p>
+        </div> */}
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
       </div>
     </div>
   );
