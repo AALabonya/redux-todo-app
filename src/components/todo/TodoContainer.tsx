ae820270f9c085bd5111ff9b287135c5043287ad
AALabonya <1aklimakter@gmail.com>
2024-07-05 10:33:29 -0700

    added

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -2,9 +2,13 @@ import { useAppSelector } from "@/redux/hooks";
 import AddtodoModal from "./AddtodoModal";
 import TodoCard from "./TodoCard";
 import { TodoFilter } from "./TodoFilter";
+import { useGetTodosQuery } from "@/redux/api/api";
 
 const TodoContainer = () => {
+  //from local state
   const { todos } = useAppSelector((state) => state.todos);
+  //*From server
+  const { data: todos, isLoading, isError } = useGetTodosQuery(undefined);
   return (
     <div className="px-2.5 py-3.5 border rounded-md">
       <div className="flex justify-between rounded-md mb-5">
