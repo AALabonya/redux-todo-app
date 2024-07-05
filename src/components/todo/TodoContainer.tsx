dd32cb6140c8bc2a0730f76256294a877610c485
AALabonya <1aklimakter@gmail.com>
2024-07-04 23:12:41 -0700

    added

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,11 +1,15 @@
 import TodoCard from "./TodoCard";
-import { Button } from "@/components/ui/button";
+
 const TodoContainer = () => {
   return (
     <div className="px-2.5 py-3.5 border rounded-md">
-      <div className="flex justify-between">
-        <Button>Add todo</Button>
-        <Button>Filter</Button>
+      <div className="flex justify-between rounded-md mb-5">
+        <button className="bg-primary-gradient text-white rounded-lg p-2">
+          Add todo
+        </button>
+        <button className="bg-primary-gradient p-2 rounded-lg text-white">
+          Filter
+        </button>
       </div>
       <div className="bg-primary-gradient w-full space-y-3 h-[500px] rounded-xl p-5">
         {/* <div className="bg-white rounded-md  items-center p-3 text-center flex justify-center">
