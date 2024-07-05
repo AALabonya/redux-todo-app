3400637b14e038feeb5f921bc9db31b72401cde2
AALabonya <1aklimakter@gmail.com>
2024-07-05 09:36:52 -0700

    added

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -6,14 +6,22 @@ type TTodoCardProps = {
   id: string;
   title: string;
   description: string;
+  isCompleted: boolean;
 };
-const TodoCard = ({ title, description, id }: TTodoCardProps) => {
+const TodoCard = ({ title, description, id, isCompleted }: TTodoCardProps) => {
   const dispatch = useAppDispatch();
   return (
     <div className="bg-white rounded-md flex justify-between items-center p-3">
       <input type="checkbox" name="" id="" />
       <p>{title}</p>
       {/* <p>Time</p> */}
+      <div>
+        {isCompleted ? (
+          <p className="text-green-600">Done</p>
+        ) : (
+          <p className="text-yellow-500">pending</p>
+        )}
+      </div>
       <p>{description}</p>
       <div className="flex gap-5">
         <Button onClick={() => dispatch(removeTodo(id))}>del</Button>
