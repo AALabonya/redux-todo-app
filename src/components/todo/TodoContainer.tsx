0a1d5435e214646525348dc59c49d4c6ace6d12e
AALabonya <1aklimakter@gmail.com>
2024-07-04 23:54:59 -0700

    added

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -4,12 +4,9 @@ const TodoContainer = () => {
   return (
     <div className="px-2.5 py-3.5 border rounded-md">
       <div className="flex justify-between rounded-md mb-5">
-        <button className="bg-primary-gradient text-white rounded-lg p-2">
-          Add todoco
-        </button>
-        <button className="bg-primary-gradient p-2 rounded-lg text-white">
+        {/* <button className="bg-primary-gradient p-2 rounded-lg text-white">
           Filter
-        </button>
+        </button> */}
       </div>
       <div className="bg-primary-gradient w-full space-y-3 h-[500px] rounded-xl p-5">
         {/* <div className="bg-white rounded-md  items-center p-3 text-center flex justify-center">
