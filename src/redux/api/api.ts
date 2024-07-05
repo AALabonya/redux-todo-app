11ceb7606ff07d6dcc83be525b78bcac482cedf5
AALabonya <1aklimakter@gmail.com>
2024-07-05 10:26:38 -0700

    added

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -1,6 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseApi = createApi({
-    reducerPath:"ggg",
-    baseQuery: fetchBaseQuery({baseUrl:})
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  endpoints: (builder) => ({
+    //query holo get korar jonno
+    //post korar jonno muation
+    getTodos: builder.query({
+      query: () => ({
+        url: "/tasks",
+        method: "GET",
+      }),
+    }),
+  }),
 });
